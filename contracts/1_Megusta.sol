e08d77089f0034167fc22dd99c33bf7d5b15d6c5
Nambi <harrisonlvl2@gmail.com>
2024-12-31 13:56:16 +0530

    added the distribute method

--- contracts/1_Megusta.sol
+++ contracts/1_Megusta.sol
@@ -2,8 +2,9 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Megusta {
+contract Megusta is AccessControl{
 
     struct tournament {
         uint tid; // if of the tournament
@@ -22,44 +23,62 @@ contract Megusta {
         uint timestamp;
     }
 
+    constructor(){
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
     uint count = 0;
-    address ADMIN =   0xD0dC8A261Ad1B75A92C5e502AE10c3Fde042b879;  
+    bytes32 public constant ADMIN_ROLE =   keccak256("ADMIN_ROLE");  
    
     mapping (uint => tournament) public tournaments;
     mapping (uint => performance[]) public performances;
     mapping (uint => mapping (uint => uint)) public last_created; //(gid => (duration => timestamp))
-    mapping (address => mapping (uint => uint)) public tickets; // user add => (game id => number)
     
-    function create_tournament(uint g_id, uint dur, uint mp) public{
+    function addManager(address target) public onlyRole(DEFAULT_ADMIN_ROLE){
+        _grantRole(ADMIN_ROLE, target);
+    }
+    
+    function create_tournament(uint g_id, uint dur, bool mp_flag) public{
         uint c = count + 1;
         require (dur == 1 || dur == 2 || dur == 3 || dur == 4, "duration is 1, 2, 3, or 4");
-        require (mp == 20 || mp == 50 || mp == 100 || mp == 500 || mp == 0, "min participants is 20, 50, 100, 500, or 0 for open");
         uint last_instance = last_created[g_id][dur];
         uint timenow = block.timestamp;
         uint st = 0;
         uint ed = 0;
+        uint mp = 0;
 
         if (dur == 2){
             require (last_instance + 24*60*60 < timenow, "such tournament already ongoing");
             st = timenow;
             ed = timenow + (24*60*60);
+            if (mp_flag){
+                mp = 20;
+            }
 
         }
         else if (dur == 3){
             require (last_instance + 7*24*60*60 < timenow, "such tournament already ongoing");
             st = timenow;
             ed = timenow + (7*24*60*60);
+            if (mp_flag){
+                mp = 100;
+            }
         }
         else if (dur == 4){
             require (last_instance + 30*24*60*60 < timenow, "such tournament already ongoing");
             st = timenow;
             ed = timenow + (30*24*60*60);
+            if (mp_flag){
+                mp = 500;
+            }
         }
         else {
             require (last_instance + 60 < timenow, "last such tournament created within a minute back");
             st = timenow;
             ed = timenow + (60*60);
+            if (mp_flag){
+                mp = 20;
+            }
         }
 
         tournament memory t = tournament({
@@ -79,20 +98,36 @@ contract Megusta {
     
 
    
-    function buy_tickets(uint n, uint g_id) public payable {
+    function play_game(uint t_id) public payable {
+        tournament memory t = tournaments[t_id];
+        require (t.gid != 0, "tournament does not exist");
+        uint n = 0;
+        if (t.duration == 1){
+            n = 1;
+        }
+        else if (t.duration == 2){
+            n = 5;
+        }
+        else if (t.duration == 3){
+            n = 10;
+        }
+        else if (t.duration == 4){
+            n = 15;
+        }
+        else {
+            n = 0;
+        }
+        require (n != 0, "something went wrong");
         require(
             msg.value >= n * 0.005 ether, 
             "each ticket costs 0.005 ether, are you out of balance?"
         );
-        uint tix = tickets[msg.sender][g_id];
-        tickets[msg.sender][g_id] = tix + n;
     }
 
   
    
 
-    function record_score(uint t_id, address pl, uint dur, uint g_id, uint scr, uint scr_tp) public {
-        require (msg.sender == ADMIN, "this is an admin only function");
+    function record_score(uint t_id, address pl, uint scr, uint scr_tp) public onlyRole(ADMIN_ROLE) {
         performance memory p = performance({
             tid: t_id,
             player: pl,
@@ -100,26 +135,7 @@ contract Megusta {
             score_type: scr_tp,
             timestamp: block.timestamp
         });
-        uint tix = tickets[pl][g_id];
-        if (dur == 1){
-            require (tix >= 1, "need a ticket to play");
-            tickets[pl][g_id] = tix - 1;
-        }
-        else if (dur == 2){
-            require (tix >= 5, "need 5 tickets to play");
-            tickets[pl][g_id] = tix - 5;
-        }
-        else if (dur == 3){
-            require (tix >= 10, "need 10 tickets to play");
-            tickets[pl][g_id] = tix - 10;
-        }
-        else if (dur == 4){
-            require (tix >= 15, "need 15 tickets to play");
-            tickets[pl][g_id] = tix - 15;
-        }
-        else {
-            require (false, "something went wrong");
-        }
+        
 
         performance[] storage plist = performances[t_id];
         plist.push(p);
@@ -127,6 +143,48 @@ contract Megusta {
         
         performances[t_id] = plist;
 
+    }
+
+    function distribute_rewards(uint t_id, address[] memory winners) public onlyRole(ADMIN_ROLE) {
+        tournament memory t = tournaments[t_id];
+        uint timenow = block.timestamp;
+        require (t.end <= timenow, "tournament is ongoing");
+        require (t.gid != 0, "tournament does not exist");
+        
+        uint n = 0;
+        if (t.duration == 1){
+            n = 1;
+        }
+        else if (t.duration == 2){
+            n = 5;
+        }
+        else if (t.duration == 3){
+            n = 10;
+        }
+        else if (t.duration == 4){
+            n = 15;
+        }
+        else {
+            n = 0;
+        }
+        require (n != 0, "something went wrong");
+
+        performance[] memory plist = performances[t_id];
+        require (plist.length / 2 == winners.length, "not enough chosen winners");
+        if (plist.length < t.min_participants){
+            uint i = 0;
+            while (i < plist.length){
+                payable(plist[i].player).transfer(n * 0.005 ether);
+                i = i + 1;
+            }
+        }
+        else {
+            uint i = 0;
+            while(i < plist.length){
+                payable(winners[i]).transfer(n * 0.01 ether);
+                i = i + 1;
+            }
+        }
     }
 
     
